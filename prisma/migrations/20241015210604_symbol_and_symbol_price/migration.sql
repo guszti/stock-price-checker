66f7e7873c41a864e9b2120660b12adb89520691
Guszti <agoston.kiraly@outlook.hu>
2024-10-15 23:27:09 +0200

    symbol and symbol_price migration

---
+++ prisma/migrations/20241015210604_symbol_and_symbol_price/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE "symbol" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "created_at" TIMESTAMPTZ(1) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "symbol_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "symbol_price" (
+    "id" SERIAL NOT NULL,
+    "symbol_id" INTEGER NOT NULL,
+    "created_at" TIMESTAMPTZ(1) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "symbol_price_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "symbol_name_key" ON "symbol"("name");
+
+-- AddForeignKey
+ALTER TABLE "symbol_price" ADD CONSTRAINT "symbol_price_symbol_id_fkey" FOREIGN KEY ("symbol_id") REFERENCES "symbol"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
